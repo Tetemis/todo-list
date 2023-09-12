2c3500fb37b308f08910da78c2b81d64e37e8c6f
Tetemis <113144690+Tetemis@users.noreply.github.com>
2023-09-11 21:16:46 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {Tarefas} from './components/Tarefas';
+
+export function App(){
+  return(
+    <div>
+    <h1>Andressa's ToDo List</h1>
+    <Tarefas/>
+    </div>
+  )
+}
---
+++ src/components/Tarefas.tsx
@@ -0,0 +1,8 @@
+export function Tarefas(){
+  return(
+    <div>
+      <label>Adicione uma nova tarefa:</label>
+      <input type='text' name="novatarefa"/>
+    </div>
+  )
+}
